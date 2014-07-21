d2dd5ebdb928735e963ad2078ac480174b520543
kicoe <kicoe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-21 09:23:10 +0000

    Update version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-resume-shortcode/trunk@952060 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wordpress-resume-shortcode.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+Plugin Name: Wordpress Resume Shortcode
+Plugin URI: http://www.patrickroux.fr
+Description: A wordpress plugin allowing the use of shortcodes to build a resume page. For help on how to use it, go to Settings > WP Resume SC.
+Version: 1.0
+Author: Patrick Roux
+Author URI: http://www.patrickroux.fr
+License: GPL2
+*/
+
+
+
+class wordpress_resume_shortcode
+{
+    public function __construct()
+    {
+        include_once plugin_dir_path( __FILE__ ).'/actions/wprsc-addshortcode.php';
+		new wprsc_addshortcode();
+
+        include_once plugin_dir_path( __FILE__ ).'/actions/wprsc-addmenu.php';
+		new wprsc_addmenu();
+    }
+}
+
+new wordpress_resume_shortcode();
+
+
+?>
